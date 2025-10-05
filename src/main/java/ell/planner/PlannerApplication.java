18b905dbbc50805b61bfc29366eee96675973e6a
변시윤 <sy8389b@miridih.com>
2025-10-05 17:05:24 +0900

    EDP-01 프로젝트 초기화

---
+++ src/main/java/ell/planner/PlannerApplication.java
@@ -0,0 +1,13 @@
+package ell.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerApplication.class, args);
+	}
+
+}
